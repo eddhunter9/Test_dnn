655f0e26f4668027e7a55f9323b2bcd8433954b6
eddhunter <mietekkledzik@gmail.com>
2025-12-16 22:25:03 +0100

    update

--- perceptron.py
+++ perceptron.py
@@ -8,8 +8,32 @@ class Perceptron:
         self.bias = 0
 
     def forward(self, x = list[float]):
-        weighted_sum = np.dot(x, self.weights) + self.bias
+        weighted_sum = np.dot(x, self.weights) + self.bias #iloczyn skalarny
         output = self.activation_func(weighted_sum)
 
         return output
+
+    def train(self, X_train: list[list[float]], y_expected: list[float], n_iter: int, learn_rate: float):
+        number_inp = len(X_train[0])
+        self.weights = np.random.randn(number_inp)
+        self.bias = np.random.randn()
+
+        for _ in range(n_iter):
+            for i, x in enumerate(X_train):
+                y_predicted = self.forward(x)
+                error = y_expected[i] - y_predicted
+                correction = error * learn_rate
+                self.weights = self.weights + correction * x
+                self.bias = self.bias + correction
+
+    def predict(self, X: list[list[float]]):
+        predictions = []
+        for _, x in enumerate(X):
+            output = self.forward(x)
+            predictions.append(output)
+
+        return predictions
+
+
+
 
---
+++ predictor.py
@@ -0,0 +1,5 @@
+from perceptron import Perceptron
+from sklearn import datasets
+
+iris_dataset = datasets.load_iris()
+
